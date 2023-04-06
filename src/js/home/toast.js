1f8a843c3ddc3319e6fa157f7d28d9bb7a1e35b3
Ứng Trí <lamcongtri200@gmail.com>
2023-04-06 23:48:00 +0700

    update dangnhap 1

--- src/js/home/toast.js
+++ src/js/home/toast.js
@@ -2,6 +2,8 @@ let success = document.querySelectorAll('#success');
 let warning = document.querySelectorAll('#warning');
 let login = document.querySelectorAll('#login');
 let logout = document.querySelectorAll('#logout');
+let phone = document.querySelectorAll('#phone');
+let notpass = document.querySelectorAll('#notpass');
 let notifications = document.querySelector('.notifications');
 
 
@@ -80,6 +82,52 @@ logout.forEach(button => {
         let icon = 'fa-solid fa-circle-check';
         let title = 'Thành công';
         let text = 'Đăng ký thành công.';
+        let newToast = document.createElement('div');
+    newToast.innerHTML = `
+        <div class="toast ${type}">
+                <i class="${icon}"></i>
+                <div class="content">
+                    <div class="title">${title}</div>
+                    <span>${text}</span>
+                </div>
+                <i class="close fa-solid fa-xmark"
+                onclick="(this.parentElement).remove()"
+                ></i>
+            </div>`;
+
+    notifications.appendChild(newToast);
+    newToast.timeOut = setTimeout(() => newToast.remove(), 5000)
+    }
+});
+phone.forEach(button => {
+    button.onclick = function(){
+        let type = 'phone';
+        let icon = 'fa-solid fa-circle-check';
+        let title = 'Thành công';
+        let text = 'Gửi liên hệ thành công.';
+        let newToast = document.createElement('div');
+    newToast.innerHTML = `
+        <div class="toast ${type}">
+                <i class="${icon}"></i>
+                <div class="content">
+                    <div class="title">${title}</div>
+                    <span>${text}</span>
+                </div>
+                <i class="close fa-solid fa-xmark"
+                onclick="(this.parentElement).remove()"
+                ></i>
+            </div>`;
+
+    notifications.appendChild(newToast);
+    newToast.timeOut = setTimeout(() => newToast.remove(), 5000)
+    }
+});
+notpass.forEach(button => {
+    button.onclick = function(){
+        let type = 'notpass';
+        let icon = 'fa-solid fa-circle-check';
+        let title = 'HOÀN THÀNH';
+        let text = 'Tạo mật khẩu mới thành công';
         let newToast = document.createElement('div');
     newToast.innerHTML = `
         <div class="toast ${type}">
