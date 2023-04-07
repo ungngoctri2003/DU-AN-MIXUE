958fcea655dbb1ccad8ee774412c74d2d43df668
Ứng Trí <lamcongtri200@gmail.com>
2023-04-07 23:38:34 +0700

    update gio hang

--- src/js/home/index.js
+++ src/js/home/index.js
@@ -25,20 +25,4 @@ function showSlides(n) {
     dots[slideIndex - 1].className += " active";
 }
 
-// Line
-// let line = document.querySelector('.line');
-// const items = document.querySelectorAll('.header__item');
-// const itemActive = document.querySelector('.header__item.active');
-
-// line.style.left = itemActive.offsetLeft + "px";
-// line.style.width = itemActive.offsetWidth + "px";
-
-// items.forEach((item, index) => {
-// item.onclick = function () {
-// itemActive.classList.remove("active");
-// line.style.left = this.offsetLeft + "px";
-// line.style.width = this.offsetWidth + "px";
-// this.classList.add("active")
-// }
-// });
 
--- src/js/home/toast.js
+++ src/js/home/toast.js
@@ -1,5 +1,4 @@
 let success = document.querySelectorAll('#success');
-let warning = document.querySelectorAll('#warning');
 let login = document.querySelectorAll('#login');
 let logout = document.querySelectorAll('#logout');
 let phone = document.querySelectorAll('#phone');
@@ -13,29 +12,6 @@ success.forEach(button => {
         let icon = 'fa-solid fa-circle-check';
         let title = 'Thành công';
         let text = 'Đặt hàng thành công.';
-        let newToast = document.createElement('div');
-    newToast.innerHTML = `
-        <div class="toast ${type}">
-                <i class="${icon}"></i>
-                <div class="content">
-                    <div class="title">${title}</div>
-                    <span>${text}</span>
-                </div>
-                <i class="close fa-solid fa-xmark"
-                onclick="(this.parentElement).remove()"
-                ></i>
-            </div>`;
-
-    notifications.appendChild(newToast);
-    newToast.timeOut = setTimeout(() => newToast.remove(), 5000)
-    }
-});
-warning.forEach(button => {
-    button.onclick = function(){
-        let type = 'warning';
-    let icon = 'fa-solid fa-triangle-exclamation';
-    let title = 'Xóa';
-    let text = 'Xóa sản phẩm thành công';
         let newToast = document.createElement('div');
     newToast.innerHTML = `
         <div class="toast ${type}">
