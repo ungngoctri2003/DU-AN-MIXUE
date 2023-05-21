9332c86f705ecf819086812d0e8866d8f85127d2
Ứng Trí <lamcongtri200@gmail.com>
2023-05-21 07:03:35 +0700

    update scroll_top

---
+++ src/js/home/Scroll_top.js
@@ -0,0 +1,12 @@
+var offset = 300;
+var duration = 300;
+$(function (){
+$(window).scroll(function () {
+if ($(this).scrollTop() > offset)
+$('#top-up').fadeIn(duration);else
+$('#top-up').fadeOut(duration);
+});
+$('#top-up').click(function () {
+$('body,html').animate({scrollTop: 0}, duration);
+});
+});
